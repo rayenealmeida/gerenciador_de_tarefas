7234bd95b4e53930dec4adbc95bd697a47d31a00
almeidarayene <almeidarayene@gmail.com>
2023-04-27 23:18:14 -0300

    widget_test

--- todox/test/widget_test.dart
+++ todox/test/widget_test.dart
@@ -1,19 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:todox/main.dart';
+import 'package:todox/models/gerenciador_tarefas.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Cria uma instância de GerenciadorTarefas para passar para MyApp
+    final gerenciadorTarefas = GerenciadorTarefas();
+
+    // Adiciona algumas tarefas para o gerenciador de tarefas
+    expect(find.text('Tarefa 1'), findsOneWidget);
+    expect(find.text('Tarefa 2'), findsOneWidget);
+
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(gerenciadorTarefas: gerenciadorTarefas));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
