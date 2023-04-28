287617e20f94f14ff6124b3c1c39399117c1afb4
almeidarayene <almeidarayene@gmail.com>
2023-04-27 23:18:03 -0300

    null_error

--- todox/lib/main.dart
+++ todox/lib/main.dart
@@ -1,115 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:todox/models/gerenciador_tarefas.dart';
 
 void main() {
-  runApp(const MyApp());
+  final gerenciadorTarefas = GerenciadorTarefas();
+
+  gerenciadorTarefas.adicionarUsuario('João');
+  gerenciadorTarefas.adicionarTarefa(
+      gerenciadorTarefas.usuarios[0], 'Comprar leite');
+  gerenciadorTarefas.adicionarTarefa(
+      gerenciadorTarefas.usuarios[0], 'Fazer exercícios');
+  gerenciadorTarefas
+      .marcarTarefaConcluida(gerenciadorTarefas.usuarios[0].tarefas[1]);
+
+  gerenciadorTarefas.adicionarUsuario('Maria');
+  gerenciadorTarefas.adicionarTarefa(
+      gerenciadorTarefas.usuarios[1], 'Ler livro');
+  gerenciadorTarefas.adicionarTarefa(
+      gerenciadorTarefas.usuarios[1], 'Assistir filme');
+  gerenciadorTarefas
+      .marcarTarefaConcluida(gerenciadorTarefas.usuarios[1].tarefas[0]);
+
+  runApp(MyApp(gerenciadorTarefas: gerenciadorTarefas));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GerenciadorTarefas gerenciadorTarefas;
 
-  // This widget is the root of your application.
+  const MyApp({Key? key, required this.gerenciadorTarefas}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ToDoX',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(gerenciadorTarefas: gerenciadorTarefas),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  final GerenciadorTarefas gerenciadorTarefas;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const HomePage({Key? key, required this.gerenciadorTarefas})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('ToDoX'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: ListView.builder(
+          itemCount: gerenciadorTarefas.usuarios.length,
+          itemBuilder: (BuildContext context, int index) {
+            final usuario = gerenciadorTarefas.usuarios[index];
+            return ExpansionTile(
+              title: Text(usuario.nome),
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: usuario.tarefas.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final tarefa = usuario.tarefas[index];
+
+                    return ListTile(
+                      title: Text(tarefa.nome),
+                      trailing: Checkbox(
+                        value: tarefa.concluida,
+                        onChanged: (value) {
+                          if (value!) {
+                            gerenciadorTarefas.marcarTarefaConcluida(tarefa);
+                          } else {
+                            gerenciadorTarefas.desmarcarTarefaConcluida(tarefa);
+                          }
+                        },
+                      ),
+                      subtitle: tarefa.dataConclusao != null
+                          ? Text('Concluída em ${tarefa.dataConclusao}')
+                          : tarefa.estaAtrasada()
+                              ? const Text('Atrasada!')
+                              : null,
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return const AlertDialog(
+                  title: Text('Nova tarefa'),
+                );
+              });
+        }));
   }
 }
